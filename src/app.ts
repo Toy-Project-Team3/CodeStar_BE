6df3dbaa3bf8c5f6fad9dca50c4ed1db2f958b4a
seon-mikim <tjsalsun@gmail.com>
2023-04-17 23:07:59 +0900

    í³feat: express 초기 설정

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import express from 'express';
+import { myDataBase } from "../db"
+import cors from 'cors'
+
+export const tokenList ={}
+
+myDataBase
+    .initialize()
+    .then(() => {
+        console.log("DataBase has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during DataBase initialization:", err)
+    })
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors({
+  origin: true
+}))
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
