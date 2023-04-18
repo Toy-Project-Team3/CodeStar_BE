ca2666d32cd24c094873373194ddc59376d721a2
seon-mikim <tjsalsun@gmail.com>
2023-04-18 23:48:11 +0900

    í³feat: Entity Column 추가
    - bio, creditScore, postList Column 추가

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,25 @@
-import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm'
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm'
+import { Post } from './Post'
 
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn('uuid')
-    id: string
+    id:string
     @Column({unique: true})
     userId: string
     @Column({unique: true})
-    username:string
+    userName:string
+    @Column()
+    profileImg: string
     @Column()
     password:string
+    @Column()
+    bio:string
+    @Column()
+    creditScore: number
+    @OneToMany(type => Post, post => post.author)
+    postList:Post[]
     @CreateDateColumn()
     createdAt: Date
     @UpdateDateColumn()
