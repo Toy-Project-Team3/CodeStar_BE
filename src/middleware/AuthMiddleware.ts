4f4245c6d6aaa413b94d55ca8f2eb12ffa82143f
seon-mikim <tjsalsun@gmail.com>
2023-04-19 00:44:38 +0900

    í³feat: AuthMiddleware 함수 생성
    - 미들웨어 생성

---
+++ src/middleware/AuthMiddleware.ts
@@ -0,0 +1,24 @@
+import { Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { JwtRequest, TokenPayload } from '../interface/interfaces';
+dotenv.config();
+
+export class AuthMiddleware {
+  static verifiyToken = (req: JwtRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+      return res.status(403).send('A token is required');
+    }
+
+    try {
+      const decoded = verify(token, process.env.SECRTE_ATOKEN) as TokenPayload;
+      req.decoded = decoded;
+    } catch (err) {
+      return res.status(401).send('Invalid Token');
+    }
+    return next();
+  };
+}
