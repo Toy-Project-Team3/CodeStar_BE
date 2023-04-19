898a4636c796b68da7205b7513fad9f7e1d34a96
seon-mikim <tjsalsun@gmail.com>
2023-04-19 20:44:05 +0900

    ✏️typo: 오타 수정
    - env 오타 수정

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -8,13 +8,12 @@ export class AuthMiddleware {
   static verifiyToken = (req: JwtRequest, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-
+   
     if (!token) {
       return res.status(403).send('A token is required');
     }
-
     try {
-      const decoded = verify(token, process.env.SECRTE_ATOKEN) as TokenPayload;
+      const decoded = verify(token, process.env.SECRET_ATOKEN) as TokenPayload;
       req.decoded = decoded;
     } catch (err) {
       return res.status(401).send('Invalid Token');
