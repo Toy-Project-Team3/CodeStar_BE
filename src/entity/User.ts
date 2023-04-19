d6f7151c8a2344d5347a8a9f04a1752291364665
seon-mikim <tjsalsun@gmail.com>
2023-04-19 19:23:49 +0900

    í·️style: 코드 가독성을 위해서 줄 정리
    - 가독성을 위해 정리 및 주석 추가

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,27 +1,26 @@
-import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm'
-import { Post } from './Post'
-
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Post } from './Post';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    id:string
-    @Column({unique: true})
-    userId: string
-    @Column({unique: true})
-    userName:string
-    @Column()
-    profileImg: string
-    @Column()
-    password:string
-    @Column()
-    bio:string
-    @Column()
-    creditScore: number
-    @OneToMany(type => Post, post => post.author)
-    postList:Post[]
-    @CreateDateColumn()
-    createdAt: Date
-    @UpdateDateColumn()
-    updatedAt: Date
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ unique: true })
+  userId: string;
+  @Column({ unique: true })
+  userName: string;
+  @Column()
+  password: string;
+  @Column()
+  profileImg: string;
+  @Column()
+  bio: string;
+  @Column()
+  creditScore: number;
+  @OneToMany(() => Post, (post) => post.author) //,{ cascade: ["insert", "update", 'remove'] }
+  postList: Post[];
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
