a9fc18ce875739869762c5f942779d3a7402a0ed
seon-mikim <tjsalsun@gmail.com>
2023-04-21 00:52:57 +0900

    :notebook_with_decorative_cover: feat: 로그아웃 라우터 구현

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -5,5 +5,6 @@ const routes = Router();
 
 routes.post('/register', UserController.register);
 routes.post('/login', UserController.login);
+routes.post('/logout', UserController.logout)
 
 export default routes;
