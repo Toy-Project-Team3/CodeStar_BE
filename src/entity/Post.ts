27adb4f33a53e69f36b4edf4483fc66ccf6146c2
seon-mikim <tjsalsun@gmail.com>
2023-04-20 10:37:26 +0900

    í³feat: Post Entity Column 속성 추가 및
    - BaseEntity 사용

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,14 +1,28 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+
+} from 'typeorm';
 import { User } from './User';
 
+
+
 @Entity()
-export class Post {
+export class Post extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   postId: string;
   @Column()
   title: string;
   @Column()
   content: string;
+  @Column("simple-array", { nullable: true })
+  tags: string[];
   @ManyToOne(() => User, (user) => user.postList)
   author: User;
   @CreateDateColumn()
