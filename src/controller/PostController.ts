63287fe1ba3a84f7730227d428ccdc535e8c2b19
seon-mikim <tjsalsun@gmail.com>
2023-04-21 01:01:18 +0900

    :notebook_with_decorative_cover: feat: status와 오류메세지 작성

--- src/controller/PostController.ts
+++ src/controller/PostController.ts
@@ -4,7 +4,6 @@ import { User } from '../entity/User';
 import { JwtRequest } from '../interface/interfaces';
 import { Response, Request } from 'express';
 
-
 export class PostController {
   static createPost = async (req: JwtRequest, res: Response) => {
     const { title, content, tags } = req.body;
@@ -14,6 +13,11 @@ export class PostController {
       where: { id: userId },
     });
 
+  if(!title&& !content) {
+    return res.status(400).json({ message: '게시글 제목과 내용을 입력해주세요.' })
+  }
+
+
     const post = new Post();
     post.title = title;
     post.content = content;
@@ -21,7 +25,9 @@ export class PostController {
     post.tags = tags;
 
     const result = await myDataBase.getRepository(Post).insert(post);
-    res.status(201).send(result);
+  
+      res.status(201).json({ message: '게시글이 작성완료되었습니다.' });
+ 
   };
 
   static getPosts = async (req: Request, res: Response) => {
@@ -36,7 +42,7 @@ export class PostController {
         author: true,
       },
     });
-    res.send(results);
+    res.status(200).send(results);
   };
   static getPost = async (req: Request, res: Response) => {
     const results = await myDataBase.getRepository(Post).findOne({
@@ -51,36 +57,45 @@ export class PostController {
         author: true,
       },
     });
-   
-    res.send(results);
+    try{
+      res.status(200).send(results);
+
+    }catch (error){
+      res.status(404).json({message: '해당 게시글을 찾을 수 없습니다.'})
+    }
   };
 
   static updatePost = async (req: JwtRequest, res: Response) => {
-    const {id: userId} = req.decoded
+    const { id: userId } = req.decoded;
 
     const currentPost = await myDataBase.getRepository(Post).findOne({
-      where: {postId: req.params.postId},
+      where: { postId: req.params.postId },
       relations: {
-        author: true
-      }
-    })
-    
+        author: true,
+      },
+    });
+
     if (!currentPost) {
-      return res.status(404).send("Post not found");
+      return res.status(404).json({ message: '해당 게시물을 찾을 수 없습니다.' });
     }
 
-    if(userId !== currentPost.author.id) {
-      return res.status(401).send('No Permission')
+    if (userId !== currentPost.author.id) {
+      return res.status(401).json({ message: '작성자 본인이 아닙니다.' });
     }
-    const {title, content, tags} = req.body
-    const newPost = new Post()
-    newPost.title = title
-    newPost.content = content 
-    newPost.tags = tags
+    const { title, content, tags } = req.body;
+    const newPost = new Post();
+    newPost.title = title;
+    newPost.content = content;
+    newPost.tags = tags;
 
-    const results = await myDataBase.getRepository(Post).update({postId:req.params.postId},newPost)
-    res.send({message:"게시글이 수정되었습니다."})
-  }
+    const results = await myDataBase.getRepository(Post).update({ postId: req.params.postId }, newPost);
+    try{
+
+      res.status(200).json({ message: '게시글이 수정되었습니다.' });
+    }catch(err) {
+      res.status(500).json({message: '게시글 작성 중 오류가 발생했습니다.'})
+    }
+  };
 
   static deletePost = async (req: JwtRequest, res: Response) => {
     const { id: userId } = req.decoded;
@@ -91,14 +106,13 @@ export class PostController {
       },
     });
     if (!currentPost) {
-      return res.status(404).send("해당 ID의 게시물을 찾을 수 없습니다.");
+      return res.status(404).json({ message: '해당 게시물을 찾을 수 없습니다.' });
     }
     if (userId !== currentPost.author.id) {
-      return res.status(401).send("작성자 본인이 아닙니다.");
+      return res.status(401).json({ message: '작성자 본인이 아닙니다.' });
     }
-   
-      const results = await myDataBase.getRepository(Post).delete({postId:req.params.postId});
-      res.status(204).send({message: "삭제 완료되었습니다."})
-    
+
+    const results = await myDataBase.getRepository(Post).delete({ postId: req.params.postId });
+    res.status(204).json({ message: '삭제 완료되었습니다.' });
   };
 }
--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -12,11 +12,23 @@ export class UserController {
   static register = async (req: Request, res: Response) => {
     const { userId, password, userName } = req.body;
 
+    const userIdRegx = /^[a-zA-Z0-9]{4,16}$/.test(userId);
+    if (!userIdRegx) {
+      return res.status(400).json({
+        message: 'ID는 숫자와 영문 대소문자 4~16자까지만 가능합니다.',
+      });
+    }
+
+    const userNameRegex = /^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣._-]{3,20}$/;
+    if (!userNameRegex.test(userName)) {
+      return res.status(400).json({ message: '유효한 닉네임이 아닙니다.' });
+    }
+
     const existUser = await myDataBase.getRepository(User).findOne({
       where: [{ userId }, { userName }],
     });
     if (existUser) {
-      return res.status(400).json({ error: 'Duplicate User' });
+      return res.status(400).json({ message: '이미 가입된 회원입니다.' });
     }
     const user = new User();
     user.userId = userId;
@@ -41,12 +53,12 @@ export class UserController {
     });
 
     if (!user) {
-      return res.status(400).json({ error: 'User not find' });
+      return res.status(400).json({ message: '회원가입이 되어있지 않습니다.' });
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
-      return res.status(400).json({ error: 'Invalid Password' });
+      return res.status(400).json({ message: '아이디나 비밀번호가 일치하지 않습니다.' });
     }
 
     const accessToken = generateAccessToken(user.id, user.userId, user.userName);
@@ -55,5 +67,10 @@ export class UserController {
     const decoded = verify(accessToken, process.env.SECRET_ATOKEN);
     res.cookie('refereshToken', refreshToken, { path: '/', httpOnly: true, maxAge: 3600 * 24 * 30 * 1000 });
     res.send({ content: decoded, accessToken });
+  };
+
+  static logout = (req: Request, res: Response) => {
+    res.clearCookie('refreshToken', { path: '/' });
+    res.status(204).json({message: '로그 아웃되었습니다.'});
   };
 }
