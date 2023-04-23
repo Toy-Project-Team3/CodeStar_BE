b73fc18ff5bcda36abe207a6bd07d0423caef541
seon-mikim <tjsalsun@gmail.com>
2023-04-23 23:20:59 +0900

    í³feat: 내 블로그 및 특정
    - 유저 블로그 조회 api 구현
    - 라우터 주소 수정

--- src/router/post.ts
+++ src/router/post.ts
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 import { AuthMiddleware } from '../middleware/AuthMiddleware';
 import { PostController } from '../controller/PostController';
+import { CommentController } from '../controller/CommentController';
+
+
 
 const routes = Router();
 
 routes.post('', AuthMiddleware.verifiyToken, PostController.createPost);
 routes.get('', PostController.getPosts);
-routes.get('/:postId', PostController.getPost);
-routes.put('/:postId', AuthMiddleware.verifiyToken, PostController.updatePost)
+routes.put('/:title', AuthMiddleware.verifiyToken, PostController.updatePost)
 routes.delete('/:postId', AuthMiddleware.verifiyToken, PostController.deletePost)
+routes.get('/:userId', PostController.getAuthorPosts)
+routes.get('/:userId/:title', PostController.getAuthorPost);
+routes.post('/:userId/:title/comments', AuthMiddleware.verifiyToken, CommentController.createComment)
+
+
+
 export default routes;
