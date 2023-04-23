5b8c609bf30c60198fa45872f03dcfd2be15a4f1
seon-mikim <tjsalsun@gmail.com>
2023-04-23 23:12:27 +0900

    feat: getAuthorPosts와 getAuthorPost 생성
    - getAuthorPosts( 내 블로그와 특정 유저의 블로그) 함수,
      getAthorPost (내 블로그와 특정 블로그에 단일 게시글 조회)함수 생성

--- src/controller/PostController.ts
+++ src/controller/PostController.ts
@@ -5,95 +5,168 @@ import { JwtRequest } from '../interface/interfaces';
 import { Response, Request } from 'express';
 
 export class PostController {
+  /**게시글 생성*/
   static createPost = async (req: JwtRequest, res: Response) => {
-    const { title, content, tags } = req.body;
+    const { title, content } = req.body;
     const { id: userId } = req.decoded;
 
     const user = await myDataBase.getRepository(User).findOne({
       where: { id: userId },
     });
 
-  if(!title&& !content) {
-    return res.status(400).json({ message: '게시글 제목과 내용을 입력해주세요.' })
-  }
-
+    if (title === '' && content === '') {
+      return res.status(400).json({ message: '게시글 제목과 내용을 입력해주세요.' });
+    }
 
     const post = new Post();
     post.title = title;
     post.content = content;
     post.author = user;
-    post.tags = tags;
 
     const result = await myDataBase.getRepository(Post).insert(post);
-  
-      res.status(201).json({ message: '게시글이 작성완료되었습니다.' });
- 
-  };
 
+    res.status(201).json({ message: '게시글이 작성완료되었습니다.' });
+  };
+  /** 모든 게시글 조회*/
   static getPosts = async (req: Request, res: Response) => {
     const results = await myDataBase.getRepository(Post).find({
       select: {
         author: {
           id: true,
+          userId: true,
           userName: true,
+          profileImg:true
         },
       },
       relations: {
         author: true,
+        commentList: true,
       },
     });
     res.status(200).send(results);
   };
-  static getPost = async (req: Request, res: Response) => {
-    const results = await myDataBase.getRepository(Post).findOne({
-      where: { postId: req.params.postId },
+
+  static getAuthorPosts = async (req: Request, res: Response) => {
+    const user = await myDataBase.getRepository(User).findOne({
+      where: { userId: req.params.userId },
       select: {
-        author: {
-          id: true,
-          userName: true,
-        },
+        id: true,
+        userId: true,
+        userName: true,
+        postList: true,
+        profileImg: true,
+        bio: true,
+        commentList: true,
+        creditScore:true,
+
       },
-      relations: {
-        author: true,
+      relations:{
+        postList:true,
+        commentList:true,
+      }
+    });
+  
+    const results = await myDataBase.getRepository(Post).find({
+      where: { author: {userId:user.userId} },
+      select: {
+              postId:true, 
+              title:true,
+              content:true, 
+              createdAt:true, 
+              author:{
+                  userId: true,
+                  userName:true,
+                  profileImg:true,
+                  creditScore:true,
+                  bio:true
+              },
+      },
+      relations:{
+        author:true,
+        commentList:true
       },
     });
-    try{
+    console.log(res)
+    try {
       res.status(200).send(results);
-
-    }catch (error){
-      res.status(404).json({message: '해당 게시글을 찾을 수 없습니다.'})
+    } catch (err) {
+      res.status(404).json({ message: '게시글들을 찾을 수 없습니다.' });
     }
   };
 
+  static getAuthorPost = async (req: Request, res: Response) => {
+    const user = await myDataBase.getRepository(User).findOne({
+      where: { userId: req.params.userId },
+    });
+    const results = await myDataBase.getRepository(Post).findOne({
+      where: { author: {userId: user.userId},title: req.params.title },
+      select: {
+        author:{
+          id: true,
+          userId: true,
+          userName: true,
+          profileImg: true,
+          creditScore:true,
+          
+        },
+        commentList:{
+          commentId:true,
+          content:true,
+          createdAt: true,
+          author:{
+            id: true,
+            userId: true,
+            userName: true,
+            profileImg: true
+            },
+          }
+      
+      },
+      relations: {
+        author: true,
+        commentList: {
+          author:true
+        },
+      },
+    });
+    console.log(results)
+    try {
+      res.status(200).send(results);
+    } catch (err) {
+      res.status(404).json({ message: '게시글들을 찾을 수 없습니다.' });
+    }
+  };
+  /**게시글 수정*/
   static updatePost = async (req: JwtRequest, res: Response) => {
-    const { id: userId } = req.decoded;
-
+    const { userId: userId } = req.decoded;
+    const {title, content} = req.body;
+    const user = await myDataBase.getRepository(User).findOne({
+      where: {userId: req.params.userId },
+    });
     const currentPost = await myDataBase.getRepository(Post).findOne({
-      where: { postId: req.params.postId },
+      where: { author:{userId: user.userId}, title: req.params.title},
       relations: {
         author: true,
       },
     });
-
+  
     if (!currentPost) {
       return res.status(404).json({ message: '해당 게시물을 찾을 수 없습니다.' });
     }
 
-    if (userId !== currentPost.author.id) {
+    if (userId !== currentPost.author.userId) {
       return res.status(401).json({ message: '작성자 본인이 아닙니다.' });
     }
-    const { title, content, tags } = req.body;
     const newPost = new Post();
     newPost.title = title;
     newPost.content = content;
-    newPost.tags = tags;
 
-    const results = await myDataBase.getRepository(Post).update({ postId: req.params.postId }, newPost);
-    try{
-
+    const results = await myDataBase.getRepository(Post).update({ title: req.params.title }, newPost);
+    console.log(results)
+    try {
       res.status(200).json({ message: '게시글이 수정되었습니다.' });
-    }catch(err) {
-      res.status(500).json({message: '게시글 작성 중 오류가 발생했습니다.'})
+    } catch (err) {
+      res.status(500).json({ message: '게시글 작성 중 오류가 발생했습니다.' });
     }
   };
 
@@ -115,4 +188,6 @@ export class PostController {
     const results = await myDataBase.getRepository(Post).delete({ postId: req.params.postId });
     res.status(204).json({ message: '삭제 완료되었습니다.' });
   };
+
+
 }
